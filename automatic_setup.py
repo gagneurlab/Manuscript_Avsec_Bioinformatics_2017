4260e313972e9f768ad2042ac8c18d9933ff0219
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-12-04 13:43:47 +0100

    Now automatically cleaning the tab files.

--- automatic_setup.py
+++ automatic_setup.py
@@ -112,6 +112,32 @@ with Pool(cpu_count()) as p:
     list(tqdm(p.imap(downloader, jobs), total=len(jobs)))
 
 
+def clear_file_chromosomes(filename: str, target: str):
+    """Removes rows with chromosomes beginning with given target.
+        filename:str, the path to the file to clear.
+        target:str, the target for the rows to remove.
+    """
+    df = pd.read_csv(filename, sep='\t', header=None)
+    a = np.array(df)
+    mask = np.array([row.startswith(target) for row in a[:, 0]])
+    pd.DataFrame(a[~mask]).to_csv(filename, sep='\t', header=None)
+
+
+def clear_directory_chromosomes(directory: str, target: str = "chrUn_"):
+    """Removes rows with chromosomes beginning with given target.
+        directory:str, the path to the directory to clear.
+        target:str, the target for the rows to remove.
+    """
+    [
+        clear_file_chromosomes(
+            "{directory}/{file}".format(directory=directory, file=file),
+            target) for file in tqdm(next(os.walk(directory))[2]) if file.endswith(".tab")
+    ]
+
+print("Cleaning tab files.")
+clear_directory_chromosomes(raw_path)
+
+
 print("Switching metadata.tsv file.")
 if os.path.exists(metadata_path):
     os.remove(metadata_path)
